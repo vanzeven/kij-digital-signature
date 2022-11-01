bb4c3c25a3041283f84d5a1a7bac3cfd4714e963
ALuthfiH <ahluthfiha24@gmail.com>
2022-11-01 20:23:00 +0700

    Add HashFile Class

---
+++ HashFile.py
@@ -0,0 +1,14 @@
+import hashlib
+import sys
+import os
+
+class HashFile:
+    def startHashFile(filepath):
+        with open(filepath, mode='rb') as f:
+                data = f.read()
+                hash_obj = hashlib.sha256()
+                hash_obj.update(data)
+                generated_checksum = hash_obj.hexdigest()
+
+        return (generated_checksum)
+        ##print(f"Generated Checksum: {generated_checksum}")
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 from KeyPairGenerator import KeyPairGenerator
+from HashFile import HashFile
 
 key_pair_generator = KeyPairGenerator()
 (private_key, public_key) = key_pair_generator.generateKeyPair()
 
 print(private_key)
 print(public_key)
+
+filepath = r'D:\Visual Studio Code\Python Programming\kij-digital-signature\AWS Academy Learner Lab - Student Guide.pdf'
