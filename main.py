85bb9b069f721dc3c3ccb4b782dfcec250056682
Syafiqjos <syafiqjos@gmail.com>
2022-11-02 07:13:40 +0700

    feat: add sign and verify embed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import re
 import rsa
+import shutil
+import base64
 
 from HashFile import HashFile
 from KeyPairGenerator import KeyPairGenerator
@@ -24,6 +26,10 @@ def parse_args():
                         help="Load the required configurations and create the certificate")
     parser.add_argument('-i', '--input_path', dest='input_path', type=is_valid_path,
                         help="Enter the path of the file or the folder to process")
+    parser.add_argument('-e', '--embed_path', dest='embed_path', type=is_valid_path,
+                        help="Enter the path of the pdf file to be sign embed")
+    parser.add_argument('-x', '--verify_embed', dest='verify_embed', type=is_valid_path,
+                        help="Enter the path of the embedded pdf file to be verify")
     parser.add_argument('-v', '--verify', dest='verify', type=is_valid_path,
                         help="Enter the path of the file or the folder to verify")
 
@@ -83,7 +89,6 @@ def signer(path):
      key.close()
      # msg.close()
 
-
 def verify(path):
     key = open('public_key.pem', 'rb')
     pubKey = rsa.PublicKey.load_pkcs1(key.read())
@@ -101,6 +106,81 @@ def verify(path):
     key.close()
     # msg.close()
     signature.close()
+
+def signer_embed(path, hash):
+    key = open('private_key.pem', 'rb')
+    privKey = rsa.PrivateKey.load_pkcs1(key.read())
+    
+    msg2 = hash
+    signature = sign_sha1(msg2, privKey)
+    sign = signature
+
+    signature_path = 'signature.txt'
+
+    writeb(signature_path, sign)
+
+    write_pdf_embed(path, signature_path)
+
+    key.close()
+
+def write_pdf_embed(pdf_path, signature_path):
+    (basename, ext) = os.path.splitext(pdf_path)
+    filename = basename + '_signed' + ext
+
+    shutil.copyfile(pdf_path, filename)
+
+    pdf_file = open(filename, 'ab')
+    signature_file = open(signature_path, 'rb')
+
+    pdf_file.write(base64.b64encode(signature_file.read()))
+    # pdf_file.write(b'x')
+
+    pdf_file.close()
+    signature_file.close()
+
+def verify_embed(path):
+    key = open('public_key.pem', 'rb')
+    pubKey = rsa.PublicKey.load_pkcs1(key.read())
+
+    key.close()
+
+    combined_file = open(path, 'rb')
+
+    combined_bytes = combined_file.read()
+
+    combined_file.close()
+
+    splited_chunks = combined_bytes.split(b'EOF')
+
+    signature_chunk = splited_chunks[-1]
+
+    splited_chunks.pop()
+    pdf_chunk = b''
+
+    for i in range(len(splited_chunks)):
+        pdf_chunk += splited_chunks[i]
+        pdf_chunk += b'EOF'
+
+    new_pdf_file = open('new_unsigned_pdf.pdf', 'wb')
+
+    original_pdf = pdf_chunk
+    embeded_signature = base64.b64decode(signature_chunk)
+
+    new_pdf_file.write(original_pdf)
+    new_pdf_file.close()
+
+    hasher = HashFile()
+    original_signed = hasher.startHashFile('new_unsigned_pdf.pdf')
+
+    os.remove('new_unsigned_pdf.pdf')
+
+    cek = verify_sha1(original_signed, embeded_signature, pubKey)
+
+    if cek:
+        print('Signature verified!')
+    else:
+        print('Could not verify the message signature.')
+
 
 
 if __name__ == '__main__':
@@ -125,5 +205,22 @@ if __name__ == '__main__':
             verify(hash)
         # If Folder Path
         elif os.path.isdir(args['verify']):
+            # Process a folder
+            print("not a file")
+    elif args['embed_path']:
+        # If File Path
+        if os.path.isfile(args['embed_path']):
+            hasher = HashFile()
+            hash = hasher.startHashFile(args['embed_path'])
+            signer_embed(args['embed_path'], hash)
+        # If Folder Path
+        elif os.path.isdir(args['embed_path']):
+            # Process a folder
+            print("not a file")
+    elif args['verify_embed']:
+        if os.path.isfile(args['verify_embed']):
+            verify_embed(args['verify_embed'])
+        # If Folder Path
+        elif os.path.isdir(args['verify_embed']):
             # Process a folder
             print("not a file")
